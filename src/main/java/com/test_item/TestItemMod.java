6456e2198581215900dc92adbc714ba9fd817e8c
Fing2525 <phuritatfing@gmail.com>
2026-02-04 17:02:09 +0700

    add 1 block and 1 item

---
+++ src/main/java/com/test_item/TestItemMod.java
@@ -0,0 +1,51 @@
+package com.test_item;
+
+
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.material.MapColor;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import com.test_item.ModItem;
+
+
+@Mod(TestItemMod.MOD_ID)
+public class TestItemMod {
+
+    public static final String MOD_ID = "test_item";
+
+    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MOD_ID);
+    public static final DeferredBlock<Block> EXAMPLE_BLOCK = BLOCKS.registerSimpleBlock("test_block", p -> p.mapColor(MapColor.STONE));
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MOD_ID);
+    public static final DeferredItem<BlockItem> EXAMPLE_BLOCK_ITEM = ITEMS.registerSimpleBlockItem("test_block", EXAMPLE_BLOCK);
+        public static  DeferredItem<Item> TEST_ITEM = TestItemMod.ITEMS.registerSimpleItem("item_test");
+
+    public TestItemMod(IEventBus modEventBus) {
+
+        BLOCKS.register(modEventBus);
+        ITEMS.register(modEventBus);
+
+        modEventBus.addListener(this::addCreative);
+    }
+
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(TEST_ITEM.get());
+        
+            
+        }
+        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+            event.accept(EXAMPLE_BLOCK_ITEM.get());
+        }
+        
+    }
+
+}
+
